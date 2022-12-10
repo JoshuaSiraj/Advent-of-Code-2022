3a2ed95a326a6d0c70881ea860d239ecfff3a44b
JoshuaSiraj <72413722+JoshuaSiraj@users.noreply.github.com>
2022-12-09 23:56:41 -0500

    Day 9

---
+++ Day 9/day9_1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+check = {(0, 0)}
+
+def main():
+    global check
+
+    tail = [0, 0]
+    head = [0, 0]
+    options = {'R': [1, 0], 'L': [-1, 0], 'U': [0, 1], 'D': [0, -1]}
+
+    with open("input.txt", 'r') as f:
+        instructions = f.readlines()
+
+    for instruction in instructions:
+        direction = instruction[0]
+        distance = int(instruction[2:])
+
+        move(head, tail, distance, options[direction])
+
+    print(len(check))
+
+def move(head, tail, distance, delta):
+    global check
+
+    for n in range(1, distance+1):
+        head[0]+=delta[0]
+        head[1]+=delta[1]
+
+        if not_adjacent(head, tail):
+            if (head[0] != tail[0] or head[1] != tail[1]):
+                tail[0]+=sign(head[0] - tail[0])
+                tail[1]+=sign(head[1] - tail[1])
+            else:
+                tail[0]+=delta[0]
+                tail[1]+=delta[1]
+            
+            check.add((tail[0], tail[1]))
+
+def not_adjacent(head, tail):
+    dist_x = head[0] - tail[0]
+    dist_y = head[1] - tail[1]
+
+    if (abs(dist_x) < 2 and abs(dist_y) < 2):
+        return False
+    
+    return True   
+
+def sign(x):
+    return -1 if x < 0 else (1 if x > 0 else 0)
+
+if __name__ == '__main__':
+    main()
